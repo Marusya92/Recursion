56d20eef369fd7e6ab81af0d3143c91f10fd7801
Mariya <marusya16081992@mail.ru>
2023-12-21 01:25:40 +0300

    решение первой задачи

---
+++ Object1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int M = 5;
+        int N = 10;
+
+        PrintNaturalNumbersInRange(M, N);
+    }
+
+    static void PrintNaturalNumbersInRange(int start, int end)
+    {
+        if (start > end)
+        {
+            return;
+        }
+
+        Console.WriteLine(start);
+
+        PrintNaturalNumbersInRange(start + 1, end);
+    }
+}
