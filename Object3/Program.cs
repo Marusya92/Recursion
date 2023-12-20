9c5b2c31614769cc5a837ce0f460d4d30fc6cfec
Mariya <marusya16081992@mail.ru>
2023-12-21 01:33:30 +0300

    решение третьей задачи

---
+++ Object3/Program.cs
@@ -0,0 +1,25 @@
+﻿// See Задайте произвольный массив.
+// Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 6, 9, 12, 15, 20 };
+
+        PrintArrayReverse(array, array.Length - 1);
+    }
+
+    static void PrintArrayReverse(int[] array, int index)
+    {
+        if (index < 0)
+        {
+            return;
+        }
+        Console.WriteLine(array[index]);
+
+        PrintArrayReverse(array, index - 1);
+    }
+}
